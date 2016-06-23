637ca5f37cb656265d67eb27694608eaea7b8c48
Eric Snell <eric_snell21@yahoo.com>
2016-06-23 11:40:22 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(event){
+
+
+	$.ajax({
+		type: 'GET',
+		url: 'http://gateway.marvel.com:80/v1/public/events/37?apikey=6801b29bb8b5052bd8f877a6282a1119'
+
+	})
+	.done(function(result){
+		console.log(result);
+	});
+
+
+
+
+
+})
