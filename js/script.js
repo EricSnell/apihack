601893e8f7030895f892a59eb25e18c1ca098bc3
Eric Snell <eric_snell21@yahoo.com>
2016-06-23 16:44:30 -0400

    hide/show divs commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,55 @@
 $(document).ready(function(event){
+	hideQuiz();
 
-
-	$.ajax({
-		type: 'GET',
-		url: 'http://gateway.marvel.com:80/v1/public/events/37?apikey=6801b29bb8b5052bd8f877a6282a1119'
-
-	})
-	.done(function(result){
-		console.log(result);
+	$('.game-button').click(function(event){
+		showQuiz();
+		ajaxCall();
 	});
 
+	$('#quiz-form').submit(function(event){
+		event.preventDefault();
+
+	});
+
+
+	
+
+
+
+
+
+/*========= FUNCTIONS =========*/
+
+	function ajaxCall() {
+
+		$.ajax({
+			type: 'GET',
+			url: 'http://gateway.marvel.com:80/v1/public/events/37?apikey=6801b29bb8b5052bd8f877a6282a1119'
+
+		})
+		.done(function(result){
+			console.log(result);
+		});
+	}
+
+	function hideQuiz(){
+		$('.quiz-area').hide();
+		$('.toggle-hidden').show();
+	}
+
+	function showQuiz(){
+		$('.toggle-hidden').hide();
+		$('.quiz-area').show();
+	}
+
+
+
+
+
+
+
+
+
+
+
 });
-
-
-
-
