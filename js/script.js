af4ee0d9dc0f87c2001c9e6c011f2317e3a28981
Sierra Gregg <sierragregg@Gmail.com>
2016-06-24 09:26:59 -0500

    I think I have fixed my git mistake

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 $(document).ready(function(event) {
+  /* ============ VARIABLES ============== */
+  var baseUrl = 'http://gateway.marvel.com:80/v1/public/';
+  var apiKey = '6801b29bb8b5052bd8f877a6282a1119';
+
   hideQuiz();
 
-  console.log(randomLetter());
- 
-  console.log(randomItemFromArray([1,4,56,8]));
 
 
   $('.game-button').click(function(event) {
     showQuiz();
-    ajaxCall();
+    // ajaxCall();
 
   });
 
   $('#quiz-form').submit(function(event) {
     event.preventDefault();
-
+    runGame();
   });
 
   /*========= FUNCTIONS =========*/
   function runGame() {
   	var randLet = randomLetter();
-  	var currentCharacter;
-
+  	var currentCharacter = ajaxCall('characters', {apikey: apiKey, nameStartsWith: randLet});
   }
 
 
-  function ajaxCall() {
+  function ajaxCall(endpoint, queryString) {
 
     $.ajax({
         type: 'GET',
-        url: 'http://gateway.marvel.com:80/v1/public/events/37?apikey=6801b29bb8b5052bd8f877a6282a1119'
-
+        url: baseUrl + endpoint,
+        data: queryString
       })
       .done(function(result) {
         var temp = randomItemFromArray(result.data.results);
@@ -50,7 +50,7 @@ $(document).ready(function(event) {
 
   function randomLetter() {
     return String.fromCharCode(randomRangeInclusive('a'.charCodeAt(0),'z'.charCodeAt(0)));
-  } 
+  }
 
 /* Random number from min (inclusive) to max (exclusive). 
    There is a chance min will appear as a random number, but max will never be returned. */
