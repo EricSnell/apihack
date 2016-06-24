afe926a7bb30c4a12bccd1bb70c38758094d70b3
Eric Snell <eric_snell21@yahoo.com>
2016-06-24 12:53:05 -0400

    question functions created

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(event) {
   var baseUrl = 'http://gateway.marvel.com:80/v1/public/';
   var apiKey = '6801b29bb8b5052bd8f877a6282a1119';
   var gameCharacter;
+  var quizForm = $('#quiz-form');
+  var questionNumber = 0;
 
   hideQuiz();
 
@@ -19,6 +21,8 @@ $(document).ready(function(event) {
   /* Listens for a click on the Next button */
   $('#quiz-form').submit(function(event) {
     event.preventDefault();
+    questionArray[questionNumber]();
+    questionNumber++;
   });
 
   /*========= FUNCTIONS =========*/
@@ -27,13 +31,45 @@ $(document).ready(function(event) {
     ajaxCall('characters', {apikey: apiKey, nameStartsWith: randLet});
   }
   
+  var questionArray = [question1, question2, question3, question4, question5];
 
-  function populateQuestion() {
-    var quizForm = $('#quiz-form');
+  function question1() { 
     quizForm.find('p').text('Who is this character?');
     quizForm.find('h3').text('1 out of 6');
     quizForm.find('label[for="' + randomRangeExclusive(0,4) + '"]').text(gameCharacter.name);
     $('#left-image').attr('src', gameCharacter.thumbnail.path + '.' + gameCharacter.thumbnail.extension);
+  }
+
+  function question2() {
+    var comic = randomItemFromArray(gameCharacter.comics.items);
+    quizForm.find('p').text('What year was ' + comic.name + ' published?');
+    quizForm.find('h3').text('2 out of 6');
+    quizForm.find('label[for="' + randomRangeExclusive(0,4) + '"]').text("19 bladdy blah");
+  }
+
+  function question3(){
+    var event = randomItemFromArray(gameCharacter.events.items);
+    quizForm.find('p').text('Which one of these events were they involved in?');
+    quizForm.find('h3').text('3 out of 6');
+    quizForm.find('label[for="' + randomRangeExclusive(0,4) + '"]').text(event.name);
+  }
+
+  function question4(){
+    var series = randomItemFromArray(gameCharacter.series.items);
+    quizForm.find('p').text('Which one of these series were they a part of?');
+    quizForm.find('h3').text('4 out of 6');
+    quizForm.find('label[for="' + randomRangeExclusive(0,4) + '"]').text(series.name);
+  }
+
+  function question5(){
+    var stories = randomItemFromArray(gameCharacter.stories.items);
+    quizForm.find('p').text('Which one of these stories were they a part of?');
+    quizForm.find('h3').text('5 out of 6');
+    quizForm.find('label[for="' + randomRangeExclusive(0,4) + '"]').text(stories.name);
+  }
+
+  function question6(){
+
   }
 
   function ajaxCall(endpoint, queryString) {
@@ -49,7 +85,7 @@ $(document).ready(function(event) {
           temp = randomItemFromArray(result.data.results);
         } 
         gameCharacter = temp;
-        populateQuestion();     
+        question1();     
       });
   }
 
@@ -67,22 +103,22 @@ $(document).ready(function(event) {
     return String.fromCharCode(randomRangeInclusive('a'.charCodeAt(0),'z'.charCodeAt(0)));
   }
 
-/* Random number from min (inclusive) to max (exclusive). 
-   There is a chance min will appear as a random number, but max will never be returned. */
-function randomRangeExclusive(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min; 
-}
+  /* Random number from min (inclusive) to max (exclusive). 
+     There is a chance min will appear as a random number, but max will never be returned. */
+  function randomRangeExclusive(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min; 
+  }
 
-/* Random number from min (inclusive) to max (inclusive).
-   Both max and min have a chance of appearing as the random number.*/
-function randomRangeInclusive(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  /* Random number from min (inclusive) to max (inclusive).
+     Both max and min have a chance of appearing as the random number.*/
+  function randomRangeInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-function randomItemFromArray(array) {
-  console.log(array);
-  return array[randomRangeExclusive(0, array.length)];
-}
+  function randomItemFromArray(array) {
+    console.log(array);
+    return array[randomRangeExclusive(0, array.length)];
+  }
 
 });
 
